9d12d293a8d7c24d440da5440ac6214247a4fe49
alexqs96 <alexander.mamani.dev@gmail.com>
2023-06-11 10:59:58 -0300

    fix

--- js/index.js
+++ js/index.js
@@ -1,20 +1,28 @@
 /**
+ * Resetea el total en caso de que se cambie el valor del input
+ */
+
+function resetTotal() {
+  document.querySelector("#amount").innerText = "Total a Pagar: $";
+}
+
+/**
  * Quita las clases de validación de bootstrap de todos los inputs con la clase form-control
  */
 
-function resetInputs(){
-  document.querySelectorAll(".form-control").forEach(e => {
-    e.classList.remove("is-valid")
-    e.classList.remove("is-invalid")
-  })
+function resetInputs() {
+  document.querySelectorAll(".form-control").forEach((e) => {
+    e.classList.remove("is-valid");
+    e.classList.remove("is-invalid");
+  });
 
-  document.querySelector("#amount").innerText = "Total a Pagar: $"
+  document.querySelector("#amount").innerText = "Total a Pagar: $";
 }
 
 /**
  * Valida si un input se encuentra vacio o si no es un email valido
  * aplica la validación agregando clases de Bootstrap segun corresponda
- * 
+ *
  * @param {*} e obtiene el valor del input
  * @param {boolean} option activa la validación por email si se le indica true
  */
@@ -24,25 +32,19 @@ function checkInput(e, option) {
 
   if (option) {
     if (!validateEmail.test(e.value)) {
-      e.classList.remove("is-valid")
-      e.classList.add("is-invalid")
+      e.classList.remove("is-valid");
+      e.classList.add("is-invalid");
+    } else {
+      e.classList.remove("is-invalid");
+      e.classList.add("is-valid");
     }
-    else
-    {
-      e.classList.remove("is-invalid")
-      e.classList.add("is-valid")
-    }
-  }
-  else
-  {
+  } else {
     if (!e.value) {
-      e.classList.remove("is-valid")
-      e.classList.add("is-invalid")
-    }
-    else
-    {
-      e.classList.remove("is-invalid")
-      e.classList.add("is-valid")
+      e.classList.remove("is-valid");
+      e.classList.add("is-invalid");
+    } else {
+      e.classList.remove("is-invalid");
+      e.classList.add("is-valid");
     }
   }
 }
@@ -53,7 +55,7 @@ function checkInput(e, option) {
  */
 
 function changeTicket(value) {
-  document.querySelector('#category').value=value
+  document.querySelector("#category").value = value;
 }
 
 /**
@@ -66,23 +68,26 @@ function calculatePrice(e) {
     spectator: 0,
     student: 80,
     trainee: 50,
-    junior: 15
-  }
-  
-  const name = e.name.value
-  const surname = e.surname.value
-  const email = e.email.value
-  const discount = category[e.category.value]
-  const quantity = e.quantity.value
+    junior: 15,
+  };
+
+  const name = e.name.value;
+  const surname = e.surname.value;
+  const email = e.email.value;
+  const discount = category[e.category.value];
+  const quantity = e.quantity.value;
 
   //Valido si los campos requeridos son validos
-  checkInput(e.name)
-  checkInput(e.surname)
-  checkInput(e.email, true)
-  checkInput(e.quantity)
+  checkInput(e.name);
+  checkInput(e.surname);
+  checkInput(e.email, true);
+  checkInput(e.quantity);
 
   //Si pasa la validación se realiza el calculo del precio final
   if (name && surname && email && quantity) {
-    document.querySelector("#amount").innerText = "Total a Pagar: $"+ ((quantity * 200) - ((quantity * 200) * (discount / 100))) + " ARS"  
+    document.querySelector("#amount").innerText =
+      "Total a Pagar: $" +
+      (quantity * 200 - quantity * 200 * (discount / 100)) +
+      " ARS";
   }
 }
